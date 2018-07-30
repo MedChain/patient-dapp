ccbf6b1943b679773b72038e405e01d829bd39f5
Russell Decker <rude5139@colorado.edu>
2018-07-30 12:37:29 -0600

    navbar WIP

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { ConnectedRouter } from 'connected-react-router'
 import store, { history } from './store'
 import App from './containers/app.js'
 
-import 'react-skeleton-css/styles/skeleton.2.0.4.css'
-import 'react-skeleton-css/styles/normalize.3.0.2.css';
-import 'sanitize.css/sanitize.css'
+// import 'react-skeleton-css/styles/skeleton.2.0.4.css'
+// import 'react-skeleton-css/styles/normalize.3.0.2.css';
+// import 'sanitize.css/sanitize.css'
 import './index.css'
 
 const target = document.querySelector('#root')
