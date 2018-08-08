50195575417277420dfeaef44a62c77da0cdbf67
Steve Wishstar <steve@medchain.us>
2018-08-08 11:03:53 -0800

    adding icons for IOT buttons

--- src/containers/devices/app-select/app-select.js
+++ src/containers/devices/app-select/app-select.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import FitBit from './fitbit'
+import HealthKit from './healthkit'
+import Add from './add'
 
 import './app-select.css'
 
 const Page = props => (
   <div className="app-select">
-    <NavLink className="button" to="/devices/recent">Recent App</NavLink>
-    <NavLink className="button" to="/devices/fitbit">FitBit</NavLink>
-    <NavLink className="button" to="/devices/healthkit">HealthKit</NavLink>
-    <NavLink className="button app-add-button"  to="/devices/add">Add</NavLink>
+    <NavLink className="button app-icon-recent" to="/devices/recent">Recent App</NavLink>
+    <NavLink className="button" to="/devices/fitbit"><FitBit /></NavLink>
+    <NavLink className="button" to="/devices/healthkit"><HealthKit /></NavLink>
+    <NavLink className="button app-icon-add" to="/devices/add"><Add /></NavLink>
 
     {props.children}
   </div>
