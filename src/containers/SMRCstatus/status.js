af33615d15175900a7c0f14439cd0de41f5ce9cd
Russell Decker <rude5139@colorado.edu>
2018-08-17 10:24:04 -0500

    added html for new dropdown

--- src/containers/SMRCstatus/status.js
+++ src/containers/SMRCstatus/status.js
@@ -10,7 +10,7 @@ import { connect } from 'react-redux'
 
 import { toggleDD } from '../../modules/SMRCstatus'
 
-Modal.setAppElement('#status')
+
 
 class Page extends React.Component {
     componentDidMount() {
@@ -23,34 +23,31 @@ class Page extends React.Component {
   }
   render() {
     return (
-    <div className="nav-wrapper">
-    <li className="dropdown nav-header" onClick={this.props.toggleDD}>
-      <Modal
-            contentLabel="Status Modal"
-            isOpen={this.state.toggle}
-            // onAfterOpen={this.afterOpenModal}
-            // onRequestClose={this.closeModal}
-            // className="Modal"
-            // overlayClassName="Overlay"
-          >
-          <div
-            className="nav-link"
-            id="navbarDropdown"
-            data-toggle="dropdown"
-            // aria-haspopup="false"
-            // aria-expanded="false">
-            >
-            <img alt="" className="support-3" src="../images/connection.png" />
-          </div>
-          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a className="dropdown-item">Secure Medical Record Chain</a>
-            <a className="dropdown-item"> Private IPFS</a>
-            <div className="dropdown-divider" />
-            <Footer />
-          </div>
-      </Modal>
-        </li>
+    // <div className="dd-wrapper">
+    //   <li className="dropdown dd-header">
+    //       <div className="nav-link dd-title" id="navbarDropdown" data-toggle="dropdown">
+    //         <img alt="" className="support-3" src="../images/connection.png" />
+    //       </div>
+    //       <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+    //         <a className="dropdown-item">Secure Medical Record Chain</a>
+    //         <a className="dropdown-item"> Private IPFS</a>
+    //         <div className="dropdown-divider" />
+    //         <Footer />
+    //       </div>
+    //     </li>
+    // </div>
+    <div className="dd-wrapper">
+      <div className="dd-header">
+        <div className="dd-header-title">title</div>
+      </div>
+      <ul className="dd-list">
+        <li className="dd-list-item">1</li>
+        <li className="dd-list-item">2</li>
+        <li className="dd-list-item">2</li>
+      </ul>
     </div>
+
+
       );
     }
   }
